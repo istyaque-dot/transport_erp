fcf5e7f6b4f738d0c523ad6fd890e46367ba26c2
istyaque-dot <istyaque@gmail.com>
2026-03-22 16:24:48 +0530

    Update pod.py

--- pod.py
+++ pod.py
@@ -1,3 +1,4 @@
+import json
 import streamlit as st
 import datetime
 import time
@@ -22,7 +23,10 @@ def connect_to_sheet():
         "https://www.googleapis.com/auth/drive",
         "https://www.googleapis.com/auth/spreadsheets"
     ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("secret.json", scope)
+    # 🟢 बदलाव: अब कोड Streamlit की तिजोरी (Secrets) से चाबी लेगा
+    creds_dict = json.loads(st.secrets["gcp_service_account"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    
     client = gspread.authorize(creds)
     sheet = client.open("Khan_Transport_ERP")
     return sheet
