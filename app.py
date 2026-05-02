bbbddc4b76c090367f0584bca3287ca61b11f083
istyaque-dot <istyaque@gmail.com>
2026-05-02 18:51:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import datetime
 
-st.set_page_config(page_title="Transport ERP", page_icon="🚛", layout="wide")
+st.set_page_config(page_title="Transport ERP", page_icon="", layout="wide")
 
 # ==========================================
 # 🎨 GLOBAL CSS
